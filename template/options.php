613f266f777afacab330a1a903885ae090966337
Peter Backx <peter.backx@gmail.com>
2011-10-11 08:01:45 +0200

    add note that some JS based ads will not work in combination with Ajax

--- template/options.php
+++ template/options.php
@@ -29,7 +29,8 @@
 	<p><label for="ctopt_trackLoggedIn_yes"><input type="radio" id="ctopt_trackLoggedIn_yes" name="ctopt_trackLoggedIn" value="true" <?php if ($options['trackLoggedIn'] == "true") { echo 'checked="checked"'; }?> /> Yes</label>&nbsp;&nbsp;&nbsp;&nbsp;<label for="ctopt_trackLoggedIn_no"><input type="radio" id="ctopt_trackLoggedIn_no" name="ctopt_trackLoggedIn" value="false" <?php if ($options['trackLoggedIn'] == "false") { echo 'checked="checked"'; }?>/> No</label></p>
 
 	<h3>Use Ajax to display widget?</h3>
-	<p>This will load the widget's content via an Ajax call. If you are using any kind of caching plugin and want correct results, you need to turn this on.</p>
+	<p>This will load the widget's content via an Ajax call. If you are using any kind of caching plugin and want correct results, 
+	you need to turn this on. But keep in mind that you might need to rewrite some ads that use JavaScript.</p>
 	<p><label for="ctopt_ajaxWidget_yes"><input type="radio" id="ctopt_ajaxWidget_yes" name="ctopt_ajaxWidget" value="true" <?php if ($options['ajaxWidget'] == "true") { echo 'checked="checked"'; }?> /> Yes</label>&nbsp;&nbsp;&nbsp;&nbsp;<label for="ctopt_ajaxWidget_no"><input type="radio" id="ctopt_ajaxWidget_no" name="ctopt_ajaxWidget" value="false" <?php if ($options['ajaxWidget'] == "false") { echo 'checked="checked"'; }?>/> No</label></p>
 
     <div class="submit">
