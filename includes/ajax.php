0d1194a066891f35b0150c9b8199d863c2ea2d76
Peter Backx <peter.backx@gmail.com>
2011-10-20 08:05:13 +0200

    adding license

--- includes/ajax.php
+++ includes/ajax.php
@@ -1,4 +1,19 @@
 <?php
+/*
+Copyright 2011 Tristan Kromer, Peter Backx (email : tristan@grasshopperherder.com)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 function adherder_client_scripts() {
 	wp_enqueue_script('adherder', plugins_url('/adherder/js/adherder.js'), array('jquery'), ADHERDER_VERSION_NUM);
 	wp_localize_script( 'adherder', 'AdHerder', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
