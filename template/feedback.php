8b05cdb192376208f73eab139ce929e14593a247
Tristan Kromer <TK@TristanKromer.com>
2011-11-09 17:04:39 -0800

    Add a twitter button for the hell of it.

--- template/feedback.php
+++ template/feedback.php
@@ -1,6 +1,10 @@
 <div id="feedback" style="float:right; width: 200px; z-index: 10;" class="postbox">
 	<div class="inside">
 		<p>We value feedback, <a href="<?php echo ADHERDER_FEEDBACK_LINK; ?>">please get in touch with us.</a></p>
+		<div style="margin: 40px 0 0 0">
+		  <iframe frameborder="0" scrolling="no" allowtransparency="true" src="http://platform.twitter.com/widgets/follow_button.html#_=1313995095388&amp;align=&amp;button=blue&amp;id=twitter_tweet_button_4&amp;lang=en&amp;link_color=&amp;screen_name=TriKro&amp;show_count=&amp;show_screen_name=&amp;text_color=" class="twitter-follow-button" style="width: 300px; height: 20px; top: -15px; position: relative; left: 50px;" title=""></iframe>
+      <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    </div>
 		<p>If you're enjoying this plugin, why not donate towards future extensions?</p>
 		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="margin-left:30px;">
 			<input type="hidden" name="cmd" value="_s-xclick">
